6edb2833cbe9a0755a4ae94b2f0a6df42ae407dd
Christina Meliniotou <chris.melinioti@gmail.com>
2019-08-16 13:10:43 +0200

    frontend

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -14,19 +14,12 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    if @cocktail.save!
+    if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
       render 'new'
     end
   end
-
-
-# def destroy
-#   @dose = Dose.find(params[:id])
-#   @dose.destroy
-#   redirect_to cocktail_path(@dose.cocktail)
-# end
 
   def cocktail_params
     params.require(:cocktail).permit(:name)
